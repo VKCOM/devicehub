49086ef9641baea9e2c4575f5821024decea325f
di.smirnov <di.smirnov@corp.vk.com>
2025-05-18 16:54:54 +0300

    fix creation error

--- test/e2e/helpers/emulator/start-emulator.sh
+++ test/e2e/helpers/emulator/start-emulator.sh
@@ -3,10 +3,11 @@
 export ANDROID_SDK_ROOT=/opt/android-sdk
 export ANDROID_HOME=/opt/android-sdk
 export PATH=$PATH:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools
+export EMULATOR_ROOT="${ANDROID_HOME}/emulator"
 
 # Запуск виртуального X-сервера (нужен для рендеринга)
 #Xvfb :0 -screen 0 1280x800x16 &
 #export DISPLAY=:0
 adb devices
 # Запуск эмулятора без KVM
-emulator -avd test_avd -no-accel -no-audio -no-window -gpu swiftshader_indirect -verbose
+${EMULATOR_ROOT}/emulator -avd test_avd -no-accel -no-audio -no-window -gpu swiftshader_indirect -verbose
