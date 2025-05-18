26ac93ea5a61bf71ea821182f6464782d027a22a
di.smirnov <di.smirnov@corp.vk.com>
2025-05-18 13:56:47 +0300

    add envs

--- test/e2e/helpers/emulator/start-emulator.sh
+++ test/e2e/helpers/emulator/start-emulator.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export ANDROID_SDK_ROOT=/opt/android-sdk
+export ANDROID_HOME=/opt/android-sdk
+export PATH=$PATH:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools
+
 # Запуск виртуального X-сервера (нужен для рендеринга)
 #Xvfb :0 -screen 0 1280x800x16 &
 #export DISPLAY=:0
