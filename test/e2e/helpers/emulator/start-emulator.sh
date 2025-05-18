00a787a8dfdbfaba4fd82f1b988590d3513a30df
di.smirnov <di.smirnov@corp.vk.com>
2025-05-18 13:16:38 +0300

    change .sh and conf

--- test/e2e/helpers/emulator/start-emulator.sh
+++ test/e2e/helpers/emulator/start-emulator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Запуск виртуального X-сервера (нужен для рендеринга)
-Xvfb :0 -screen 0 1280x800x16 &
-export DISPLAY=:0
+#Xvfb :0 -screen 0 1280x800x16 &
+#export DISPLAY=:0
 adb devices
 # Запуск эмулятора без KVM
 emulator -avd test_avd -no-accel -no-audio -no-window -gpu swiftshader_indirect -verbose
