5269bdd6bafe76a32690e2317b868d5f4b0558f9
di.smirnov <di.smirnov@corp.vk.com>
2025-05-18 21:34:19 +0300

    decrease partition size

--- test/e2e/helpers/emulator/start-emulator.sh
+++ test/e2e/helpers/emulator/start-emulator.sh
@@ -6,18 +6,13 @@ export ANDROID_AVD_HOME=/tmp/avd
 export PATH=$PATH:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools
 
 echo "===> Cleaning up old AVD image files..."
-rm -f /tmp/avd/avd/test_avd.avd/*.img
-
-#echo "===> Starting Xvfb..."
-#Xvfb :0 -screen 0 1280x800x16 &
-#export DISPLAY=:0
-#sleep 2
+rm -f /tmp/avd/test_avd.avd/*.img
 
 echo "===> Starting emulator..."
-${ANDROID_HOME}/emulator/emulator -avd test_avd \
+$ANDROID_HOME/emulator/emulator -avd test_avd \
   -no-audio -no-window -gpu swiftshader_indirect \
   -partition-size 2048 -verbose &
 
 adb wait-for-device
 adb shell getprop sys.boot_completed
-echo "✅ Emulator booted (inside /tmp/avd)"
+echo "✅ Emulator booted and ready."
