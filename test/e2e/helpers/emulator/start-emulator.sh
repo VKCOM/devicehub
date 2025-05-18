f1d2f165cc407a87ed5c410e0ec67e6f50834e73
di.smirnov <di.smirnov@corp.vk.com>
2025-05-18 21:24:53 +0300

    decrease partition size

--- test/e2e/helpers/emulator/start-emulator.sh
+++ test/e2e/helpers/emulator/start-emulator.sh
@@ -2,19 +2,22 @@
 
 export ANDROID_SDK_ROOT=/opt/android-sdk
 export ANDROID_HOME=/opt/android-sdk
+export ANDROID_AVD_HOME=/tmp/avd
 export PATH=$PATH:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools
-export EMULATOR_ROOT="${ANDROID_HOME}/emulator"
 
-echo "===> Cleaning up old userdata and cache images"
-rm -f /root/.android/avd/test_avd.avd/userdata-qemu.img
-rm -f /root/.android/avd/test_avd.avd/userdata.img
-rm -f /root/.android/avd/test_avd.avd/cache.img
+echo "===> Cleaning up old AVD image files..."
+rm -f /tmp/avd/avd/test_avd.avd/*.img
 
-# Запуск виртуального X-сервера (нужен для рендеринга)
+#echo "===> Starting Xvfb..."
 #Xvfb :0 -screen 0 1280x800x16 &
 #export DISPLAY=:0
-adb devices
-# Запуск эмулятора без KVM
-${EMULATOR_ROOT}/emulator -avd test_avd -no-accel -no-audio -no-window -gpu swiftshader_indirect -verbose -partition-size 2048 &
+#sleep 2
+
+echo "===> Starting emulator..."
+${ANDROID_HOME}/emulator/emulator -avd test_avd \
+  -no-audio -no-window -gpu swiftshader_indirect \
+  -partition-size 2048 -verbose &
 
 adb wait-for-device
+adb shell getprop sys.boot_completed
+echo "✅ Emulator booted (inside /tmp/avd)"
