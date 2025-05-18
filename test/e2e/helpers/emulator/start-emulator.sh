75f3fba79d532065071a85cbb7109d1e92402765
di.smirnov <di.smirnov@corp.vk.com>
2025-05-18 20:10:00 +0300

    decrease partition size

--- test/e2e/helpers/emulator/start-emulator.sh
+++ test/e2e/helpers/emulator/start-emulator.sh
@@ -5,6 +5,11 @@ export ANDROID_HOME=/opt/android-sdk
 export PATH=$PATH:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools
 export EMULATOR_ROOT="${ANDROID_HOME}/emulator"
 
+echo "===> Cleaning up old userdata and cache images"
+rm -f /root/.android/avd/test_avd.avd/userdata-qemu.img
+rm -f /root/.android/avd/test_avd.avd/userdata.img
+rm -f /root/.android/avd/test_avd.avd/cache.img
+
 # Запуск виртуального X-сервера (нужен для рендеринга)
 #Xvfb :0 -screen 0 1280x800x16 &
 #export DISPLAY=:0
