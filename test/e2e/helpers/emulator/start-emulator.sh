1e9f865ee0fc833f483ab9cf541f71ef2bd00bbd
di.smirnov <di.smirnov@corp.vk.com>
2025-05-22 17:02:04 +0300

    simplify emulator

--- test/e2e/helpers/emulator/start-emulator.sh
+++ test/e2e/helpers/emulator/start-emulator.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools
 echo "Launching emulator..."
 emulator -avd test_avd \
   -no-accel -no-window -no-audio -gpu swiftshader_indirect \
-  -partition-size 2048 -verbose -read-only &
+  -partition-size 2048 -verbose &
 
 EMULATOR_PID=$!
 
