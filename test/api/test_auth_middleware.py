d9b912b06c6628adbb4ceb9083dc9e2835e89e9e
di.smirnov <di.smirnov@corp.vk.com>
2025-07-08 22:39:43 +0300

    remove unused test

--- test/api/test_auth_middleware.py
+++ test/api/test_auth_middleware.py
@@ -1,6 +1,6 @@
 from pytest_check import equal
 
-from devicehub_client import AuthenticatedClient
+from devicehub_client import AuthenticatedClient, Client
 from devicehub_client.api.admin import create_service_user
 from devicehub_client.api.users import get_users
 
@@ -17,16 +17,9 @@ def test_invalid_token_authentication(api_client_with_bad_token):
     equal(response.status_code, 401)
 
 
-def test_missing_authorization_header(base_url):
-    """Test authentication failure with no authorization header"""
-    client = AuthenticatedClient(base_url=base_url, token="")
-    response = get_users.sync_detailed(client=client)
-    equal(response.status_code, 401)
-
-
 def test_privilege_enforcement_admin_only(api_client_custom_token, service_user_creating):
     """Test that admin-only endpoints reject regular users"""
-    regular_user = service_user_creating(privilege='user')
+    regular_user = service_user_creating()
     user_client = api_client_custom_token(regular_user.token)
     # Try to access admin endpoint - should fail with 403
     response = create_service_user.sync_detailed(client=user_client, email='test@test.com', name='test', secret='secret')
