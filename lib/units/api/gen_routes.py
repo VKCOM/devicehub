a5f05b9648738b08d223a3b67e9286ef951cb45f
Daniil <8039921+DaniilSmirnov@users.noreply.github.com>
2025-07-21 14:58:47 +0300

    update gen_routes.py

--- lib/units/api/gen_routes.py
+++ lib/units/api/gen_routes.py
@@ -34,7 +34,7 @@ import {{{", ".join(operations.values())}}} from '{os.path.relpath(controller_f,
     for method, func in operations.items():
       route_file.write(f"""
 export function {method.replace('delete', 'del')}(req, res) {{
-    return {func}(req, res, next)
+    return {func}(req, res)
 }}
 
 """)
