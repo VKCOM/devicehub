67c68e64f2d81ef7f8fcfa7e5f9a723af1e9ff76
Daniil <8039921+DaniilSmirnov@users.noreply.github.com>
2025-08-04 16:51:44 +0300

    implement tsx

--- bin/stf.mjs
+++ bin/stf.mjs
@@ -1,3 +1,4 @@
-#!/usr/bin/env -S node --import ./lib/util/instrument.mjs
-console.log('Starting DeviceHub')
-import '../lib/cli/index.js'
+#!/usr/bin/env -S tsx --import ./lib/util/instrument.mjs
+import packageData from '../package.json' with { type: 'json' }
+console.log(`Starting DeviceHub ${packageData.version}`)
+import '../lib/cli/index.ts'
