96aec6496785bc8e9a84e3361fd8705a7855a5e5
Daniil <8039921+DaniilSmirnov@users.noreply.github.com>
2025-09-05 20:55:14 +0300

    fix /lint

--- ui/src/components/views/auth/auth-ldap-page.tsx
+++ ui/src/components/views/auth/auth-ldap-page.tsx
@@ -122,7 +122,7 @@ export const AuthLdapPage = () => {
                   </Button>
                 </FormItem>
               </FormLayoutGroup>
-              <ConditionalRender conditions={[!!authContact, authContact!= 'example.com']}>
+              <ConditionalRender conditions={[!!authContact, authContact != 'example.com']}>
                 <Button className={styles.contactButton} href={authContact} mode='link'>
                   {t('Contact Support')}
                 </Button>
